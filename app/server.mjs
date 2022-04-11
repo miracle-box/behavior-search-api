14dda11a84dc7a88da9f796bfd3e146a3783ad99
Nixieboluo <me@sagirii.me>
2022-04-12 07:50:29 +0800

    refactor(validation): move validation schema to /core directory

--- app/server.mjs
+++ app/server.mjs
@@ -2,10 +2,12 @@ import Koa from 'koa';
 import koaBody from 'koa-body';
 
 import config from '../config/config.mjs';
-import RequestException from './core/request-exception.mjs';
+
 import catchError from './middlewares/catch-error.mjs';
 import logging from './middlewares/logging.mjs';
+
 import routes from './routes/index.mjs';
+import RequestException from './core/request-exception.mjs';
 
 const app = new Koa();
 
